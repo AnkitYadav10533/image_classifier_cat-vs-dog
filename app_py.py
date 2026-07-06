9854326dfcb5b3e708b6ab8b96fcf3c837d814b2
ANKIT YADAV <adityayadav10533@gmail.com>
2026-07-06 11:39:52 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1MQyVZW_m4hJ3zJrhfQyJ5wumF9lymZvm
 """
 
-!pip install streamlit
+
 import streamlit as st
 import numpy as np
 from PIL import Image
